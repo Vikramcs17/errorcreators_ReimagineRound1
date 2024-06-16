33e27c285c96d934bc6baa675d88877ed73b19bd
Sahilk004 <148696304+Sahilk004@users.noreply.github.com>
2024-06-17 01:10:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
@@ -14,3 +14,38 @@
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  let preveiwContainer = document.querySelector('.products-preview');
+  let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+  document.querySelectorAll('.products-container .product').forEach(product => {
+      product.onclick = () => {
+          preveiwContainer.style.display = 'flex';
+          let name = product.getAttribute('data-name');
+          previewBox.forEach(preview => {
+              let target = preview.getAttribute('data-target');
+              if (name == target) {
+                  preview.classList.add('active');
+              }
+          });
+      };
+  });
+
+  previewBox.forEach(close => {
+      close.querySelector('.fa-times').onclick = () => {
+          close.classList.remove('active');
+          preveiwContainer.style.display = 'none';
+      };
+  });
+
+  // Add event listener to close the preview when clicking outside of it
+  preveiwContainer.onclick = (event) => {
+      if (!event.target.closest('.preview')) {
+          previewBox.forEach(preview => {
+              preview.classList.remove('active');
+          });
+          preveiwContainer.style.display = 'none';
+      }
+  };
+
